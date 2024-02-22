64ff090604aef2c12b2f05a921cf2acab6f0b263
kamal singh <singh540kamal@gmail.com>
2024-02-22 23:29:32 +0530

    Complete Carousel, Searchbar and responsiveness

--- js/main.mjs
+++ js/main.mjs
@@ -16,97 +16,97 @@ let currentUser = JSON.parse(localStorage.getItem("currentUser") || "[]");
 let currentUserEmail = currentUser.length === 1 ? currentUser[0].userEmail : "";
 let quantityInCart = JSON.parse(localStorage.getItem("quantity") || "{}");
 let currentUserQuantity = currentUserEmail !== "" ? quantityInCart[currentUserEmail] : [];
+const categoryClasses = ['men', 'women', 'jewellery', 'electonics'];
 let searchedProductsArray = [];
+let categoryProductArray = [];
 
 addHTML(currentUserEmail, tShirtData, content, "home");
 
 countInCart.innerText = currentUserQuantity?.length || 0;
 if (currentUser.length) {
-  loginSignUpBtn.style.display = "none";
-  userNameText.style.display = "block";
-  const username = currentUser[0].userName.substring(0, currentUser[0].userName.indexOf(' '));
-  userNameText.innerText = `Hey ${username}!`;
-  logoutBtn.style.display = "block";
-  cartIcon.addEventListener('click', () => {
-    location.href = "./pages/cart.html";
-  })
+    loginSignUpBtn.style.display = "none";
+    userNameText.style.display = "block";
+    const username = currentUser[0].userName.substring(0, currentUser[0].userName.indexOf(' '));
+    userNameText.innerText = `Hey ${username}!`;
+    logoutBtn.style.display = "block";
+    cartIcon.addEventListener('click', () => {
+        location.href = "./pages/cart.html";
+    })
 }
 else {
-  cartIcon.addEventListener('click', () => {
-    alert("First Login yourself.!");
-    location.href = "./pages/logIn.html";
-  })
+    cartIcon.addEventListener('click', () => {
+        alert("First Login yourself.!");
+        location.href = "./pages/logIn.html";
+    })
 }
 
 logoutBtn.addEventListener('click', () => {
-  location.href = "./pages/logIn.html";
-  currentUser.pop();
-  localStorage.setItem("currentUser", "[]");
+    location.href = "./pages/logIn.html";
+    currentUser.pop();
+    localStorage.setItem("currentUser", "[]");
 });
 
 searchBar.addEventListener('input', () => {
-  const val = searchBar.value;
-  content.innerHTML = "";
-  searchedProductsArray = []
-  tShirtData.filter(item => {
-    if (item.name.toLowerCase().includes(val)) searchedProductsArray.push(item);
-  });
-  if (searchedProductsArray.length) {
-    addHTML(currentUserEmail, searchedProductsArray, content, "home");
-  }
+    const val = searchBar.value;
+    content.innerHTML = "";
+    searchedProductsArray = []
+    tShirtData.filter(item => {
+        if (item.name.toLowerCase().includes(val)) searchedProductsArray.push(item);
+    });
+    if (searchedProductsArray.length) addHTML(currentUserEmail, searchedProductsArray, content, "home");
 });
 
 content.addEventListener("click", (e) => {
-  if (e.target.className === "add-cart-btn") {
-    let currentUser = JSON.parse(localStorage.getItem("currentUser") || "[]");
-    if (!currentUser.length) {
-      alert("User does not exists.!");
-      location.href = "./pages/logIn.html";
-      return;
+    if (e.target.className === "add-cart-btn") {
+        let currentUser = JSON.parse(localStorage.getItem("currentUser") || "[]");
+        if (!currentUser.length) {
+            alert("User does not exists.!");
+            location.href = "./pages/logIn.html";
+            return;
+        }
+        currentUserEmail = currentUser[0].userEmail;
+        const addCartButtonId = e.target.dataset.addcartbuttonid;
+        let index = searchedProductsArray.findIndex(ele => ele.index === Number(addCartButtonId));
+        index = index === -1 ? addCartButtonId : index;
+        addToLocalStorage(
+            currentUserEmail,
+            "home",
+            addCartButtonId,
+            tShirtData,
+            counterObj[Number(index)],
+            countInCart
+        );
+        e.target.style.display = "none";
+        const addRemoveBtns = document.querySelectorAll(".add-remove-btns"); //why array is empty when keeping it on top
+        addRemoveBtns[Number(index)].style.display = "block";
     }
-    currentUserEmail = currentUser[0].userEmail;
-    const addCartButtonId = e.target.dataset.addcartbuttonid;
-    let index = searchedProductsArray.findIndex(ele => ele.index === Number(addCartButtonId));
-    index = index === -1 ? addCartButtonId : index;
-    addToLocalStorage(
-      currentUserEmail,
-      "home",
-      addCartButtonId,
-      tShirtData,
-      counterObj[Number(index)],
-      countInCart
-    );
-    e.target.style.display = "none";
-    const addRemoveBtns = document.querySelectorAll(".add-remove-btns"); //why array is empty when keeping it on top
-    addRemoveBtns[Number(index)].style.display = "block";
-  }
-  if (e.target.className === "remove-btn") {
-    const removeButtonId = e.target.dataset.removebuttonid;
-    const addRemoveBtns = document.querySelectorAll(".add-remove-btns"); //why array is empty when keeping it on top
-    let index = searchedProductsArray.findIndex(ele => ele.index === Number(removeButtonId));
-    index = index === -1 ? removeButtonId : index;
-    removeFromLocalStorage(
-      currentUserEmail,
-      "home",
-      removeButtonId,
-      tShirtData,
-      counterObj[Number(index)],
-      addToCartBtn[Number(index)],
-      addRemoveBtns[Number(index)],
-      countInCart
-    );
-  }
-  if (e.target.className === "add-btn") {
-    const addButtonId = e.target.dataset.addbuttonid;
-    let index = searchedProductsArray.findIndex(ele => ele.index === Number(addButtonId));
-    index = index === -1 ? addButtonId : index;
-    addToLocalStorage(
-      currentUserEmail,
-      "home",
-      addButtonId,
-      tShirtData,
-      counterObj[Number(index)],
-      null
-    );
-  }
+    if (e.target.className === "remove-btn") {
+        const removeButtonId = e.target.dataset.removebuttonid;
+        const addRemoveBtns = document.querySelectorAll(".add-remove-btns"); //why array is empty when keeping it on top
+        let index = searchedProductsArray.findIndex(ele => ele.index === Number(removeButtonId));
+        index = index === -1 ? removeButtonId : index;
+        removeFromLocalStorage(
+            currentUserEmail,
+            "home",
+            removeButtonId,
+            tShirtData,
+            counterObj[Number(index)],
+            addToCartBtn[Number(index)],
+            addRemoveBtns[Number(index)],
+            countInCart
+        );
+    }
+    if (e.target.className === "add-btn") {
+        const addButtonId = e.target.dataset.addbuttonid;
+        let index = searchedProductsArray.findIndex(ele => ele.index === Number(addButtonId));
+        index = index === -1 ? addButtonId : index;
+        addToLocalStorage(
+            currentUserEmail,
+            "home",
+            addButtonId,
+            tShirtData,
+            counterObj[Number(index)],
+            null
+        );
+    }
 });
